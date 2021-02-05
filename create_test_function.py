3567886912a1213283e30c89b2ec43170a6b6259
jaredyam <jihonren@gmail.com>
2021-02-05 16:16:06 +0800

    Support creating multiple tests based on multiple selected func_names

--- create_test_function.py
+++ create_test_function.py
@@ -8,22 +8,28 @@ class CreateTestFunctionCommand(sublime_plugin.TextCommand):
     """
 
     def run(self, edit):
-        func_name = self.view.substr(self.view.sel()[0])
-        func_content = self.create_test_function(func_name)
-        endline, _ = self.view.rowcol(self.view.size())
-        point = self.view.text_point(endline, 0)
-        self.view.insert(edit, point, func_content)
+        count_regions = 0
+        for func_name_region in self.view.sel():
+            count_regions += 1
+            func_name = self.view.substr(func_name_region)
+            test_function = self.create_test_function(func_name)
 
-        if func_name:
-            point = self.view.text_point(*self.view.rowcol(self.view.size()))
-        else:
-            point = self.view.text_point(self.view.rowcol(self.view.size())[0] - 1,
-                                         9)
+            endline, _ = self.view.rowcol(self.view.size())
+            point = self.view.text_point(endline, 0)
+            self.view.insert(edit, point, test_function)
 
-        self.view.sel().clear()
-        self.view.sel().add(sublime.Region(point))
-        self.view.show(point)
+        if count_regions == 1:
+            if func_name:
+                point = self.view.text_point(
+                    self.view.rowcol(self.view.size())[0] - 1, 11)
+            else:
+                point = self.view.text_point(
+                    self.view.rowcol(self.view.size())[0] - 2, 9)
+
+            self.view.sel().clear()
+            self.view.sel().add(sublime.Region(point))
+            self.view.show(point)
 
     @staticmethod
     def create_test_function(func_name):
-        return 'def test_{}():\n    assert '.format(func_name)
+        return '\ndef test_{}():\n    assert \n'.format(func_name)
