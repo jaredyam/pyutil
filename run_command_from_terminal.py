cf48d0d365e403a58c8a25b16ca8fed7cb724c83
jaredyam <jihonren@gmail.com>
2021-02-17 23:49:04 +0800

    [Minor change] Enable Pytest shows print info with flag [-s], and more robustness

--- run_command_from_terminal.py
+++ run_command_from_terminal.py
@@ -7,15 +7,17 @@ class RunPytestCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
         osx_only()
-        selected_regions = list(self.view.sel())
+        selections = self.view.sel()
         filepath = self.view.file_name()
-        if len(selected_regions) == 1 and not self.view.substr(selected_regions[0]):
-            call_terminal(run_command='pytest {}'.format(filepath))
+        _cd_to_current_dir(filepath)
+        if len(selections) == 1 and not self.view.substr(selections[0]):
+            call_terminal(run_command='pytest -s {}'.format(filepath))
         else:
-            for region in selected_regions:
+            for region in selections:
                 text = self.view.substr(region)
-                command = 'pytest {}'.format(filepath) + ('::{}'.format(text)
-                                                          if text else '')
+                if not text:
+                    continue
+                command = 'pytest -s {}'.format(filepath) + '::{}'.format(text)
                 call_terminal(run_command=command)
 
 
@@ -23,4 +25,10 @@ class RunFileCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
         filepath = self.view.file_name()
+        _cd_to_current_dir(filepath)
         call_terminal(run_command='python {}'.format(filepath))
+
+
+def _cd_to_current_dir(filepath):
+    parentdir = '/'.join(filepath.split('/')[:-1])
+    call_terminal(run_command='cd {}'.format(parentdir))
