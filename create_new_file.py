f89f6ba1ab567de92ec232af3494eafa804ca706
jaredyam <jihonren@gmail.com>
2021-02-17 22:58:36 +0800

    [Minor change] More conscious and robustness

--- create_new_file.py
+++ create_new_file.py
@@ -6,24 +6,26 @@ import os
 class CreateNewFileCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
-        self.viewpath = self.view.file_name()
-        selected_filenames = [self.view.substr(region)
-                              for region in self.view.sel()
-                              if self.view.substr(region)]
-        if selected_filenames:
-            for new_filename in selected_filenames:
-                self.create_new_file(new_filename)
+        assert self.view.file_name() is not None, 'expected at least a base view/file opened'
+
+        filename_selections = [self.view.substr(region)
+                               for region in self.view.sel()
+                               if self.view.substr(region)]
+
+        if filename_selections:
+            for filename in filename_selections:
+                self.create_new_file(filename)
         else:
-            self.view.window().show_input_panel('New File', '.py',
+            self.view.window().show_input_panel('Create New File', '.py',
                                                 self.on_done,
                                                 None,
                                                 None)
 
-    def on_done(self, new_filename):
-        self.create_new_file(new_filename)
+    def on_done(self, filename):
+        self.create_new_file(filename)
 
-    def create_new_file(self, new_filename):
-        new_filepath = os.path.join(os.path.dirname(self.viewpath),
-                                    new_filename)
-        new_filepath = new_filepath.replace(' ', '\\ ')
-        os.system('subl {}'.format(new_filepath))
+    def create_new_file(self, filename):
+        filepath = os.path.join(os.path.dirname(self.view.file_name()),
+                                filename)
+        filepath = filepath.replace(' ', '\\ ')
+        os.system('subl {}'.format(filepath))
